10b499316b00fad5c882f111bcb590eecc93c7bf
Phankunanon <phamkunanon@gmail.com>
2017-10-04 16:02:28 +0700

    40% map player1

--- genMaze.py
+++ genMaze.py
@@ -14,22 +14,81 @@ class Maze():
             set_maze(x,y)
         
         set_maze(48,100)
+
         set_maze(80,100)
+        
         set_maze(112,100)
+        
         set_maze(144,100)
+        
         set_maze(144,132)
         set_maze(144,164)
         set_maze(144,196)
+        
         set_maze(176,196)
+        
         set_maze(208,196)
+        
         set_maze(240,100)
         set_maze(240,196)
+        
         set_maze(272,196)
         set_maze(272,100)
+        
         set_maze(304,196)
         set_maze(304,164)
         set_maze(304,132)
         set_maze(304,100)
+        
+        set_maze(336,100)
+        
+        set_maze(368,100)
+        
+        set_maze(400,100)   
+        set_maze(400,388)
+        set_maze(400,420)
+        set_maze(400,452)
+        set_maze(400,484)
+        
+        set_maze(432,100)
+        set_maze(432,484)
+
+        set_maze(464,100)
+        set_maze(464,484)
+
+
+        set_maze(496,100)
+        set_maze(496,132)
+        set_maze(496,164)
+        set_maze(496,196)
+        set_maze(496,228)
+        set_maze(496,292)
+        set_maze(496,260)
+        set_maze(496,292)
+        set_maze(496,324)
+        set_maze(496,356)
+        set_maze(496,388)
+        set_maze(496,484)
+
+        set_maze(528,100)
+        set_maze(528,292)
+        set_maze(528,484)
+        
+        set_maze(560,100)
+        set_maze(560,292)
+        set_maze(560,484)
+
+        set_maze(592,100)
+        set_maze(592,196)
+        set_maze(592,292)
+        set_maze(592,388)
+        set_maze(592,420)
+        set_maze(592,452)
+        set_maze(592,484)
+
+        set_maze(624,196)
+        
+        set_maze(656,196)
     def Set_wall(self,x,y):
         wall = arcade.Sprite("images/Block2.png", SPRITE_SCALING)
         wall.center_x = x
